8efd04d30575208b16995a86146276d1e0373d68
imseoyeong <fearless40@naver.com>
2025-04-04 16:52:03 +0900

    BookingInput update2

--- edukiosk/src/EDU03_Input.js
+++ edukiosk/src/EDU03_Input.js
@@ -7,16 +7,17 @@ export default function BookingInput() {
     const [ bookingNumber, setBookingNumber ] = useState("");
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    
     return (
       <>
-        <form>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            dispatch(findTicket(bookingNumber));
+            navigate("/print");
+        }}>
             <h2>예매티켓찾기</h2>
             <input type='text' placeholder='예매번호를 입력하세요.' value={bookingNumber} onChange={(e) => setBookingNumber(e.target.value)} />
-            <button type='button' onClick={(e) => {
-                dispatch(findTicket(bookingNumber));
-                navigate("/print");
-            }}>조회하기</button>
+            <button type='submit'>조회하기</button>
         </form>
       </>
     );
