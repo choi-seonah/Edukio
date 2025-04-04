0cd701eb6a71543108e9c0ffdb8460715bec4619
choi-seonah <dondo0412@naver.com>
2025-04-04 10:58:57 +0900

    busdestination complete

--- edukiosk/src/EDU03_DES.js
+++ edukiosk/src/EDU03_DES.js
@@ -1,13 +1,15 @@
-import bussList from "./EDU03_LIST";
+import busList from "./EDU03_LIST";
 import DesForm from "./EDU03_DES_FORM";
 import arrivetime from "./EDU03_DES_FORM";
 import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
 import React from 'react';
+import Buscheck from "./EDU03_BUSCHECK";
 
 function Des(){
     return(
             <Routes>
                 <Route path="/busdes" element={<DesForm/>}></Route>
+                <Route path="/buscheck" element={<Buscheck/>}></Route>
             </Routes>
     );
 }
--- edukiosk/src/EDU03_LIST.js
+++ edukiosk/src/EDU03_LIST.js
@@ -1,25 +1,25 @@
-const bussList=[
-    {id:1, start:"Seoul", dest:"Busan", date:"250410", time:"09:00"},
-    {id:2, start:"Seoul", dest:"Busan", date:"250411", time:"13:30"},
-    {id:3, start:"Seoul", dest:"Busan", date:"250411", time:"16:00"},
-    {id:4, start:"Seoul", dest:"Daejeon", date:"250411", time:"20:00"},
-    {id:5, start:"Seoul", dest:"Daejeon", date:"250411", time:"22:40"},
-    {id:6, start:"Seoul", dest:"Daejeon", date:"250412", time:"07:00"},
-    {id:7, start:"Busan", dest:"Seoul", date:"250413", time:"10:00"},
-    {id:8, start:"Busan", dest:"Seoul", date:"250412", time:"12:00"},
-    {id:9, start:"Daejeon", dest:"Seoul", date:"250413", time:"10:30"},
-    {id:10, start:"Daejeon", dest:"Seoul", date:"250414", time:"17:00"},
-    {id:11, start:"Daejeon", dest:"Busan", date:"250412", time:"13:00"},
-    {id:12, start:"Daejeon", dest:"Busan", date:"250413", time:"16:20"},
-    {id:13, start:"Busan", dest:"Daejeon", date:"250412", time:"08:00"},
-    {id:14, start:"Busan", dest:"Daejeon", date:"250415", time:"12:20"},
-    {id:15, start:"Seoul", dest:"Sokcho", date:"250412", time:"09:30"},
-    {id:16, start:"Seoul", dest:"Sokcho", date:"250416", time:"14:20"},
-    {id:17, start:"Seoul", dest:"Ansan", date:"250412", time:"10:20"},
-    {id:18, start:"Busan", dest:"Ulsan", date:"250410", time:"17:00"},
-    {id:19, start:"Seoul", dest:"Ulsan", date:"250413", time:"08:50"},
-    {id:20, start:"Ulsan", dest:"Daejeon", date:"250411", time:"20:20"},
-    {id:21, start:"Ulsan", dest:"Seoul", date:"250410", time:"18:10"}
+const busList=[
+    {id:"SB41009", start:"Seoul", des:"Busan", date:"2025-04-10", time:"09:00"},
+    {id:"SB41113", start:"Seoul", des:"Busan", date:"2025-04-11", time:"13:30"},
+    {id:"SB41116", start:"Seoul", des:"Busan", date:"2025-04-11", time:"16:00"},
+    {id:"SD41120", start:"Seoul", des:"Daejeon", date:"2025-04-11", time:"20:00"},
+    {id:"SD41122", start:"Seoul", des:"Daejeon", date:"2025-04-11", time:"22:40"},
+    {id:"SD41207", start:"Seoul", des:"Daejeon", date:"2025-04-12", time:"07:00"},
+    {id:"SS41209", start:"Seoul", des:"Sokcho", date:"2025-04-12", time:"09:30"},
+    {id:"SS41614", start:"Seoul", des:"Sokcho", date:"2025-04-16", time:"14:20"},
+    {id:"SA41210", start:"Seoul", des:"Ansan", date:"2025-04-12", time:"10:20"},
+    {id:"SU41308", start:"Seoul", des:"Ulsan", date:"2025-04-13", time:"08:50"},
+    {id:"BD41208", start:"Busan", des:"Daejeon", date:"2025-04-12", time:"08:00"},
+    {id:"BD41512", start:"Busan", des:"Daejeon", date:"2025-04-15", time:"12:20"},
+    {id:"BU41017", start:"Busan", des:"Ulsan", date:"2025-04-10", time:"17:00"},
+    {id:"BS41310", start:"Busan", des:"Seoul", date:"2025-04-13", time:"10:00"},
+    {id:"BS41212", start:"Busan", des:"Seoul", date:"2025-04-12", time:"12:00"},
+    {id:"DS41310", start:"Daejeon", des:"Seoul", date:"2025-04-13", time:"10:30"},
+    {id:"DS41417", start:"Daejeon", des:"Seoul", date:"2025-04-14", time:"17:00"},
+    {id:"DB41213", start:"Daejeon", des:"Busan", date:"2025-04-12", time:"13:00"},
+    {id:"DB41316", start:"Daejeon", des:"Busan", date:"2025-04-13", time:"16:20"},
+    {id:"UD41120", start:"Ulsan", des:"Daejeon", date:"2025-04-11", time:"20:20"},
+    {id:"US41018", start:"Ulsan", des:"Seoul", date:"2025-04-10", time:"18:10"}
 ];
 
-export default bussList;
+export default busList;
