ab2083018207f7f6ea47bd62dcb5baf859f3e376
imseoyeong <fearless40@naver.com>
2025-04-04 16:01:59 +0900

    BookingInput / BookingPrint_upload

--- edukiosk/src/EDU03_Input.js
+++ edukiosk/src/EDU03_Input.js
@@ -8,34 +8,16 @@ export default function BookingInput() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const selectedTicket = useSelector((state) => state.useBus.selectedTicket);
-    const message = useSelector((state) => state.useBus.message);
-
-    let ticketInfo = null;
-    if (selectedTicket) {
-        ticketInfo = (
-            <div>
-                <p>예매번호: {selectedTicket.id}</p>
-                <p>출발: {selectedTicket.start}</p>
-                <p>도착: {selectedTicket.des}</p>
-                <p>날짜: {selectedTicket.date}</p>
-                <p>시간: {selectedTicket.time}</p>
-
-                <button onClick={(e) => navigate("/print", { state: { ticket: selectedTicket } })}>출력하기</button>
-            </div>
-        );
-    }
-
     return (
       <>
         <form>
             <h2>예매티켓찾기</h2>
             <input type='text' placeholder='예매번호를 입력하세요.' value={bookingNumber} onChange={(e) => setBookingNumber(e.target.value)} />
-            <button type='button' onClick={(e) => dispatch(findTicket(bookingNumber))}>조회하기</button>       
+            <button type='button' onClick={(e) => {
+                dispatch(findTicket(bookingNumber));
+                navigate("/print");
+            }}>조회하기</button>
         </form>
-
-        {message && <p>{message}</p>}
-        {ticketInfo}
       </>
     );
   }
---
+++ edukiosk/src/EDU03_PRINT.js
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+export default function BookingPrint() {
+    const navigate = useNavigate();
+
+    const selectedTicket = useSelector((state) => state.useBus.selectedTicket);
+
+    if (!selectedTicket) {
+        return <p>예매 정보가 없습니다.</p>;
+    }
+
+    return (
+      <>
+        <div>
+            <h2>예매티켓찾기</h2>
+            <p>예매번호: {selectedTicket.id}</p>
+            <p>출발: {selectedTicket.start}</p>
+            <p>도착: {selectedTicket.des}</p>
+            <p>날짜: {selectedTicket.date} </p>
+            <p>출발 시간: {selectedTicket.time}</p>
+            <button>출력하기</button>
+        </div>
+      </>
+    );
+  }
