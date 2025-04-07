dfe4cfb63710e5b4718d1fcbebc92c985a46c0bf
imseoyeong <fearless40@naver.com>
2025-04-07 17:03:35 +0900

    way - css update

--- edukiosk/src/EDU02_Way.js
+++ edukiosk/src/EDU02_Way.js
@@ -3,11 +3,21 @@ import { Link } from "react-router-dom";
 export default function Way() {
     return (
         <>
-        <div id='way'>
-            <h1>선택해주세요</h1>
-            <ul>
-                <li><Link to='/menu'>매장에서 식사</Link></li>
-                <li><Link to='/menu'>포장 주문</Link></li>
+        <div id='way' className='inner'>
+            <h1 className='page-title'>식사 장소를 선택해주세요</h1>
+            <ul className='select-list'>
+                <li>
+                    <Link to='/menu'>
+                        <img className='ico' src="./media/ico_store.png"></img>
+                        <p className='title'>매장에서 식사</p>
+                    </Link>
+                </li>
+                <li>
+                    <Link to='/menu'>
+                        <img className='ico' src="./media/ico_togo.png"></img>
+                        <p className='title'>포장 주문</p>
+                    </Link>
+                </li>
             </ul>
         </div>
         </>
