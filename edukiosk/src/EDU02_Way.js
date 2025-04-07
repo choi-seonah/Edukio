df0442ba7b1358bc79846865af1bc2748c27f51a
imseoyeong <fearless40@naver.com>
2025-04-07 13:09:44 +0900

    메뉴 및 옵션 리스트 name 한글 변경

--- edukiosk/src/EDU02_Way.js
+++ edukiosk/src/EDU02_Way.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Way() {
     return (
         <>
-        <div>
+        <div id='way'>
             <h1>선택해주세요</h1>
             <ul>
                 <li><Link to='/menu'>매장에서 식사</Link></li>
