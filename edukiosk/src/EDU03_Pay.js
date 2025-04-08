9e27745eee45f879b1d6b7cad7225494ed4888b4
imseoyeong <fearless40@naver.com>
2025-04-08 17:58:55 +0900

    buspay css update

--- edukiosk/src/EDU03_Pay.js
+++ edukiosk/src/EDU03_Pay.js
@@ -29,37 +29,27 @@ export default function BusPay() {
 				<h2 className="page-title">최종 결제 창</h2>
 
 				<div className='pay-info'>
-					<p className='payage-title'>연령대 할인 적용</p>
-					<div className='payage'>
-						<button onClick={(e) => {
-							add("아동");
-						}}>아동</button>
-
-						<button onClick={(e) => {
-							add("청소년");
-						}}>청소년</button>
-
-						<button onClick={(e) => {
-							add("성인");
-
-						}}>성인</button>
-
-						<button onClick={(e) => {
-							add("노인");
-						}}>노인</button>
-					</div>
-					{/* <div className="bus-info">
-						<dl>
-							<dt>선택한 버스 정보</dt>
-							<dd>버스 ID: {selectedBus?.id}</dd>
-							<dd>출발지: {selectedBus?.start}</dd>
-							<dd>도착지: {selectedBus?.des}</dd>
-							<dd>출발일: {selectedBus?.date}</dd>
-							<dd>출발시간: {selectedBus?.time}</dd>
-						</dl>
-					</div> */}
 					<div className='info-box'>
-						<div>
+						<div className='left'>
+							<p className='payage-title'>연령대 할인 적용</p>
+							<div className='payage'>
+								<button onClick={(e) => {
+									add("아동");
+								}}>아동</button>
+
+								<button onClick={(e) => {
+									add("청소년");
+								}}>청소년</button>
+
+								<button onClick={(e) => {
+									add("성인");
+
+								}}>성인</button>
+
+								<button onClick={(e) => {
+									add("노인");
+								}}>노인</button>
+							</div>
 							<dl>
 								<dt>예매 정보</dt>
 								<dd>
@@ -80,17 +70,27 @@ export default function BusPay() {
 								</dd>
 							</dl>
 						</div>
-						<div className='noti'>
-							- 버스 티켓의 기본 가격은 20,000원입니다.<br />
-							- 아동, 노인: 50% 할인<br />
-							- 청소년: 30% 할인
+						<div className='help-wrap'>
+							<p className="help-title">도움말</p>
+
+							<div className="des-help">
+								연령대에 따른 추가적인 할인이 적용되기 때문에 입력하신 좌석과 일치하게 눌러주시면 됩니다.<br/><br/>
+								아동 2명에 성인 1명인 경우, 아동 버튼을 2번 누르고 성인 버튼을 1번 눌러주세요.<br/><br/>
+								잘못 누르셨다면, 결제하기 옆의 초기화 버튼을 누르고 다시 눌러주세요.<br/><br/>
+								입력이 완료되면 결제하기 버튼을 눌러서 사진과 같이 카드를 넣어주시면 됩니다.
+							</div>
 						</div>
+					</div>
+					<div className='noti'>
+						- 버스 티켓의 기본 가격은 20,000원입니다.<br />
+						- 아동, 노인: 50% 할인<br />
+						- 청소년: 30% 할인	
 					</div>
 				</div>
 
 				<div className='total-wrap'>
 
-					<h2 className='total-price'>최종금액 <span>{totalPrice}원</span></h2>
+					<h2 className='total-price'>최종금액 <span>{totalPrice.toLocaleString()}원</span></h2>
 
 					<div className='btn-wrap'>
 						<button className="submit-btn gray" onClick={(e) => {
@@ -104,16 +104,7 @@ export default function BusPay() {
 					</div>
 				</div>
 
-				<div className='help-wrap'>
-					<p className="help-title">도움말</p>
-
-					<div className="des-help">
-						<p>연령대에 따른 추가적인 할인이 적용되기 때문에 입력하신 좌석과 일치하게 눌러주시면 됩니다.</p>
-						<p>아동 2명에 성인 1명인 경우, 아동 버튼을 2번 누르고 성인 버튼을 1번 눌러주세요.</p>
-						<p>잘못 누르셨다면, 결제하기 옆의 초기화 버튼을 누르고 다시 눌러주세요.</p>
-						<p>입력이 완료되면 결제하기 버튼을 눌러서 사진과 같이 카드를 넣어주시면 됩니다.</p>
-					</div>
-				</div>
+	
 			</div>
 		</>
 	);
