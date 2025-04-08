5f1a62cf46ce9d72feae76884754b16ff61fdbe7
imseoyeong <fearless40@naver.com>
2025-04-08 16:09:13 +0900

    input, pay - css update

--- edukiosk/src/EDU03_Input.js
+++ edukiosk/src/EDU03_Input.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import busList from "./EDU03_List";
-import "./css/EDU03_Input.css";
+// import "./css/EDU03_Input.css";
 
 export default function BookingInput() {
   const [bookingNumber, setBookingNumber] = useState("");
@@ -10,29 +10,35 @@ export default function BookingInput() {
 
 
   return (
-    <div className="businput">
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        const bookingTicket = e.target.text.value;
-        const searchId = busList.find((bus)=> bus.id === bookingTicket)
-        if(searchId){
-          navigate("/busselect/businput/busprint",{state:searchId});
-        }else{
-          alert("없는 예약번호 입니다."); 
-        }
-      }}>
-        <h2 className='page-title'>예매티켓찾기</h2><hr/>
-        <input className="inputnum" type='text' name='text' placeholder='예매번호를 입력하세요.' value={bookingNumber} onChange={(e) => setBookingNumber(e.target.value)} />
-        <button className="submit-btn" type='submit'>조회하기</button>
-      </form><hr/>
+    <div id='bus-input' className="inner">
+		<form onSubmit={(e) => {
+		e.preventDefault();
+		const bookingTicket = e.target.text.value;
+		const searchId = busList.find((bus)=> bus.id === bookingTicket)
+		if(searchId){
+			navigate("/busselect/businput/busprint",{state:searchId});
+		}else{
+			alert("없는 예약번호 입니다."); 
+		}
+		}}>
+			<div className='white-box'>
+				<h1 className='page-title'>예매티켓찾기</h1>
+				<div className='input-box'>
+					<input className="inputnum" type='text' name='text' placeholder='예매번호를 입력하세요.' value={bookingNumber} onChange={(e) => setBookingNumber(e.target.value)} />
+					<button className="submit-btn" type='submit'>조회하기</button>
+				</div>
+			</div>
+		</form>
 
-      <p class="inputhelpclass">도움말</p>
-      <div class="inputhelp">
-      <p>예매번호를 입력하면 버스표를 출력할 수 있습니다.</p>
-        <p>사전예매를 하지 않으셨다면 우측 상단 홈 버튼을 눌러 초기 화면으로 돌아가세요.</p>
-        <p>예매번호를 잘못 입력하셨다면 새 창에서 확인을 누르고 다시 입력해주세요.</p>
-      </div>
-
+		<div className='help-wrap'>
+			<p className="help-title">도움말</p>
+			
+			<div className="des-help">
+				<p>예매번호를 입력하면 버스표를 출력할 수 있습니다.</p>
+				<p>사전예매를 하지 않으셨다면 우측 상단 홈 버튼을 눌러 초기 화면으로 돌아가세요.</p>
+				<p>예매번호를 잘못 입력하셨다면 새 창에서 확인을 누르고 다시 입력해주세요.</p>
+			</div>
+		</div>
     </div>
   );
 }
