74bb188dc331f6cdd8a2bacae13dab713ff1a12b
imseoyeong <fearless40@naver.com>
2025-04-09 09:48:26 +0900

    print css update

--- edukiosk/src/EDU03_Print.js
+++ edukiosk/src/EDU03_Print.js
@@ -1,5 +1,4 @@
 import { useLocation } from 'react-router-dom';
-import "./css/EDU03_Print.css";
 import { useNavigate } from 'react-router-dom';
 
 export default function BookingPrint() {
@@ -11,27 +10,48 @@ export default function BookingPrint() {
 
     return (
       <>
-        <div className="printbody">
-            <h2 className="printtitle">예매티켓찾기</h2><hr/>
-            <p>예매번호: {selectedTicket.id}</p>
-            <p>출발: {selectedTicket.start}</p>
-            <p>도착: {selectedTicket.des}</p>
-            <p>날짜: {selectedTicket.date} </p>
-            <p>출발 시간: {selectedTicket.time}</p><br/>
-            </div>
-            <div className="printbtnclass">
-            <button className="printbtn" onClick={(e) => {
-                     if (window.confirm("티켓을 출력하시겠습니까?")) {
-                        navigate("/"); 
-                      }
-                }}>출력하기</button></div>
+    	<div id='print' className="inner">
+			<h1 className='page-title'>예매티켓찾기</h1>
 
+			<div className="info-box">					
+					<dl>
+						<dt>예매번호</dt>
+						<dd>{selectedTicket.id}</dd>
+					</dl>
+					<dl>
+						<dt>출발</dt>
+						<dd>{selectedTicket.start}</dd>
+					</dl>
+					<dl>
+						<dt>도착</dt>
+						<dd>{selectedTicket.des}</dd>
+					</dl>
+					<dl>
+						<dt>출발일</dt>
+						<dd>{selectedTicket.date}</dd>
+					</dl>
+					<dl>
+						<dt>출발시간</dt>
+						<dd>{selectedTicket.time}</dd>
+					</dl>
+				</div>
+				<div className="btn-wrap">
+					<button className="submit-btn" onClick={(e) => {
+						if (window.confirm("티켓을 출력하시겠습니까?")) {
+							navigate("/"); 
+						}
+					}}>출력하기</button>
+				</div>
 
-        <p class="printhelpclass">도움말</p>
-        <div class="printhelp">
-          <p>본인의 표가 맞는지 대조해보시고, 맞다면 출력하기 버튼을 눌러주십시오.</p>
-          <p>버튼을 누르고 새 창에서 확인을 누르면, 버스 예매 연습을 마치고 홈으로 돌아갑니다.</p>
-        </div>
+				<div className='help-wrap'>
+					<p className="help-title">도움말</p>
+
+					<div className="des-help">
+						<p>본인의 표가 맞는지 대조해보시고, 맞다면 출력하기 버튼을 눌러주십시오.</p>
+						<p>버튼을 누르고 새 창에서 확인을 누르면, 버스 예매 연습을 마치고 홈으로 돌아갑니다.</p>
+					</div>
+				</div>
+		</div>
       </>
     );
   }
